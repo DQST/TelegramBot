a82fe1224e3b5f0fa5bf2102a46270f1f2809112
Dmitry Yushko <yushko07@gmail.com>
2019-03-20 12:04:38 +0300

    Refactor code

--- run.py
+++ run.py
@@ -1,80 +1,101 @@
 import asyncio
-import logging
 import logging.config
-from typing import Tuple, Any
+from typing import Tuple, Any, Optional
 
 import aiohttp
 from aiohttp import web
 
-from settings import BASE_URL, HEADERS, TOKEN, SITES_FOR_CHECK, HOST, PORT, LOGGING
+from settings import (
+    BASE_URL, HEADERS, TOKEN,
+    SITES_FOR_CHECK, HOST, PORT,
+    LOGGING, COMMANDS, WEB_HOOK_URL,
+)
 
 DELAY = 3600
 
 logger = logging.getLogger(__name__)
+logging.config.dictConfig(LOGGING)
 
 
-async def send_request(session: aiohttp.ClientSession, method: str, json_data: dict) -> Any:
-    async with session.post(url=f'{BASE_URL}{method}', json=json_data) as res:
-        return await res.json()
+class Bot(web.Application):
+    def __init__(self, *args, **kwargs):
+        super(Bot, self).__init__(*args, **kwargs)
+        self.tcp_connector = aiohttp.TCPConnector(verify_ssl=False, limit=1)
+        self.session = aiohttp.ClientSession(headers=HEADERS, connector=self.tcp_connector)
 
+    async def _call_method(self, method: str, data: Optional[dict] = None) -> Any:
+        async with self.session.post(url=f'{BASE_URL}{method}', json=data or {}) as res:
+            return await res.json()
 
-async def is_site_available(session: aiohttp.ClientSession, url: str) -> Tuple[str, bool]:
-    try:
-        async with session.get(url) as res:
-            return url, res.status == 200
-    except aiohttp.ClientConnectionError:
-        pass
-    return url, False
+    async def send_message(self, chat_id: int, message: str):
+        data = {
+            'chat_id': chat_id,
+            'text': message,
+        }
+        return await self._call_method(method='sendMessage', data=data)
 
+    async def set_web_hook(self):
+        return await self._call_method('setWebhook', {'url': WEB_HOOK_URL})
 
-async def monitoring(app: web.Application):
-    tcp_connector = aiohttp.TCPConnector(verify_ssl=False, limit=1)
-    async with aiohttp.ClientSession(headers=HEADERS,
-                                     connector=tcp_connector) as session:
-        await send_request(session, 'deleteWebhook', {})
-        await send_request(session, 'setWebhook', {'url': f'https://bot.erc20crawler.com/{TOKEN}/'})
+    async def delete_web_hook(self):
+        return await self._call_method('deleteWebhook')
+
+    async def is_site_available(self, url: str) -> Tuple[str, bool]:
+        try:
+            async with self.session.get(url) as res:
+                return url, res.status == 200
+        except aiohttp.ClientConnectionError:
+            pass
+        return url, False
+
+    async def monitoring(self):
         while True:
             tasks = [
-                app.loop.create_task(is_site_available(session, url))
-                for url in app['sites_for_checks']
+                self.loop.create_task(self.is_site_available(url))
+                for url in self['sites_for_checks']
             ]
             res = await asyncio.gather(*tasks)
             for url, status in res:
                 if not status:
                     logger.info(f'Site {url} not available.')
-                    json_data = {
-                        'chat_id': '305144318',
-                        'text': f'Site {url} not available ;(',
-                    }
-                    await send_request(session, method='sendMessage', json_data=json_data)
+                    message = f'Site {url} not available ;('
+                    await self.send_message(chat_id=305144318, message=message)
                 else:
                     logger.info(f'Site {url} available.')
                 await asyncio.sleep(DELAY)
 
+    async def handle(self, request):
+        if request.match_info.get('token') == TOKEN:
+            data = await request.json()
+            entities = data.get('entities')
+            if entities and entities.get('type') == 'bot_command':
+                text = data['text']
+                command, *args = text[1:].split(' ')
+                chat_id = data['chat']['id']
+                fun = COMMANDS.get(command)
+                if fun:
+                    res = fun(*args)
+                    await self.send_message(chat_id=chat_id, message=res)
 
-async def handle(request):
-    if request.match_info.get('token') == TOKEN:
-        res = await request.json()
-        logger.info(res)
-    return web.json_response({'status': 'ok'})
+                await self.send_message(chat_id=chat_id, message='Unknown command')
+        return web.json_response(data={'status': 'ok'})
 
+    async def startup(self):
+        await self.delete_web_hook()
+        await self.set_web_hook()
+        self['sites_for_checks'] = SITES_FOR_CHECK
+        self['monitoring_task'] = self.loop.create_task(self.monitoring())
 
-async def on_startup(app: web.Application):
-    app['sites_for_checks'] = SITES_FOR_CHECK
-    app['monitoring_task'] = app.loop.create_task(monitoring(app))
-
-
-async def on_cleanup(app):
-    app['monitoring_task'].cancel()
+    async def cleanup(self):
+        self['monitoring_task'].cancel()
+        await self.session.close()
+        await self.tcp_connector.close()
 
 
 def main():
-    logging.config.dictConfig(LOGGING)
-    app = web.Application()
-    app.router.add_post('/{token}/', handle)
-    app.on_startup.append(on_startup)
-    app.on_cleanup.append(on_cleanup)
-    web.run_app(app, host=HOST, port=PORT)
+    bot = Bot()
+    bot.router.add_post('/{token}/', bot.handle)
+    web.run_app(bot, host=HOST, port=PORT)
 
 
 if __name__ == '__main__':
